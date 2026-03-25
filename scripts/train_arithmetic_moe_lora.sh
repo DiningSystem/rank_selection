24c720157539e1a3ba1bd511a14b686f68a85389
Emkay Nguyen <81498410+DiningSystem@users.noreply.github.com>
2026-03-25 13:42:43 +0700

    Lower MoE router hidden dim in training scripts to reduce memory

---
+++ scripts/train_arithmetic_moe_lora.sh
@@ -0,0 +1,11 @@
+CUDA_VISIBLE_DEVICES=0 python train_arithmetic.py \
+  --peft_method=moe_lora \
+  --moe_expert_ranks=4,8,16,32 \
+  --moe_top_k=1 \
+  --moe_router_hidden_dim=0 \
+  --moe_expert_chunk_size=64 \
+  --moe_gradient_checkpoint_experts \
+  --lr=1e-3 \
+  --hf_fast_download \
+  --hf_preload \
+  --hf_prefer_safetensors
