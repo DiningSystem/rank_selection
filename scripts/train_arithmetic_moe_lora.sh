53b8d43f1b4d07c92135b845ec6f8792b7c88ec1
DiningSystem <81498410+DiningSystem@users.noreply.github.com>
2026-03-25 13:46:54 +0700

    fix experts2

--- scripts/train_arithmetic_moe_lora.sh
+++ scripts/train_arithmetic_moe_lora.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=0 python train_arithmetic.py \
   --peft_method=moe_lora \
-  --moe_expert_ranks=8,16,32 \
+  --moe_expert_ranks=4,8,16 \
   --moe_top_k=1 \
   --moe_router_hidden_dim=0 \
   --moe_expert_chunk_size=64 \
