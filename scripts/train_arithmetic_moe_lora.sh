ea818d07a7e7d8e3291bc2f9d735793bf37b5f73
DiningSystem <81498410+DiningSystem@users.noreply.github.com>
2026-03-27 22:38:37 +0700

    fix_training

--- scripts/train_arithmetic_moe_lora.sh
+++ scripts/train_arithmetic_moe_lora.sh
@@ -3,7 +3,6 @@ CUDA_VISIBLE_DEVICES=0 python train_arithmetic.py \
   --moe_r_max=32 \
   --moe_top_k=1 \
   --moe_router_hidden_dim=128 \
-  --gradient_checkpointing \
   --lr=1e-3 \
   --hf_fast_download \
   --hf_preload \
