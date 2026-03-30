5e1d619eeed9ff0b4dfbb85b0c74ae3801d49cae
DiningSystem <81498410+DiningSystem@users.noreply.github.com>
2026-03-30 18:04:27 +0700

    fix_cr_moe_scripts

--- scripts/train_cr_moe_lora.sh
+++ scripts/train_cr_moe_lora.sh
@@ -3,7 +3,7 @@ CUDA_VISIBLE_DEVICES=0 python train_cr.py \
   --moe_r_max=32 \
   --moe_top_k=1 \
   --moe_router_hidden_dim=128 \
-  --gradient_checkpointing \
+  --lora_alpha=32 \
   --lr=1e-3 \
   --hf_fast_download \
   --hf_preload \
