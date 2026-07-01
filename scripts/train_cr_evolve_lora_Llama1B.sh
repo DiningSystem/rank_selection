443ad307ab00ad6d05316ac1e30677220294a6b1
DiningSystem <81498410+DiningSystem@users.noreply.github.com>
2026-07-01 11:56:55 -0500

    scripts_for_llama_1B_18

--- scripts/train_cr_evolve_lora_Llama1B.sh
+++ scripts/train_cr_evolve_lora_Llama1B.sh
@@ -4,7 +4,7 @@ CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0} python train_cr.py \
   --model "meta-llama/Llama-3.2-1B" \
   --evolve_rank_delay_ratio 0.2 \
   --evolve_r_min 2 \
-  --evolve_beta 0.005 \
+  --evolve_beta 0.002 \
   --evolve_alpha_max 0.003 \
   --evolve_anneal_k 8.5e-6 \
   --evolve_gate_floor 0.12 \
@@ -18,4 +18,4 @@ CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0} python train_cr.py \
   --scheduler linear \
   --warmup_ratio 0.105 \
   --max_seq_length 256 \
-  --lr 3.97e-4
+  --lr 1e-3
