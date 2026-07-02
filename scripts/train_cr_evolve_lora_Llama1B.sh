e963365c039ccc8b9e40e0c255cdcde2081f0938
DiningSystem <81498410+DiningSystem@users.noreply.github.com>
2026-07-02 09:04:41 -0500

    scripts_for_llama_1B_23

--- scripts/train_cr_evolve_lora_Llama1B.sh
+++ scripts/train_cr_evolve_lora_Llama1B.sh
@@ -2,20 +2,20 @@ CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0} python train_cr.py \
   --adapter_type evolve_lora \
   --hf_fast_download --hf_preload --hf_prefer_safetensors "$@" \
   --model "meta-llama/Llama-3.2-1B" \
-  --evolve_rank_delay_ratio 0.2 \
+  --evolve_rank_delay_ratio 0.12 \
   --evolve_r_min 2 \
   --evolve_beta 0.001 \
-  --evolve_alpha_max 0.002 \
+  --evolve_alpha_max 0.003 \
   --evolve_anneal_k 8.5e-6 \
   --evolve_gate_floor 0.12 \
   --evolve_complexity_ema 0.9 \
   --lora_r 32 \
   --lora_alpha 32 \
-  --lora_dropout 0.04 \
+  --lora_dropout 0.05 \
   --batch_size 6 \
   --grad_acc_steps 24 \
   --epochs 2 \
   --scheduler linear \
-  --warmup_ratio 0.1 \
+  --warmup_ratio 0.08 \
   --max_seq_length 256 \
-  --lr 4.05e-4
+  --lr 3.05e-4
