061354e876e10c891ef64716183d0bf4a7898db3
DiningSystem <81498410+DiningSystem@users.noreply.github.com>
2026-07-08 21:48:42 -0500

    mistral 1

--- scripts/train_arithmetic_evolve_lora_Mistral7B.sh
+++ scripts/train_arithmetic_evolve_lora_Mistral7B.sh
@@ -5,15 +5,15 @@ CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0} python train_arithmetic.py \
   --adapter_type evolve_lora \
   --hf_fast_download --hf_preload --hf_prefer_safetensors "$@" \
   --model "mistralai/Mistral-7B-v0.1" \
-  --evolve_rank_delay_ratio 0.15 \
+  --evolve_rank_delay_ratio 0.5 \
   --evolve_r_min 2 \
-  --evolve_beta 0.05 \
+  --evolve_beta 0 \
   --evolve_alpha_max 0.01 \
   --evolve_anneal_k 5e-5 \
-  --evolve_gate_floor 0.05 \
+  --evolve_gate_floor 0.12 \
   --evolve_complexity_ema 0.9 \
   --lora_r 32 \
-  --lora_alpha 16 \
+  --lora_alpha 32 \
   --lora_dropout 0 \
   --batch_size 1 \
   --grad_acc_steps 32 \
@@ -21,4 +21,4 @@ CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0} python train_arithmetic.py \
   --scheduler cosine \
   --warmup_ratio 0.02 \
   --max_seq_length 512 \
-  --lr 1e-4
+  --lr 1e-3
