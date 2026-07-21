045bf5d3a8cd941f2b737cc21c55ed7412077aad
DiningSystem <81498410+DiningSystem@users.noreply.github.com>
2026-07-20 19:36:19 -0500

    scripts_for_gemma2_4

--- scripts/train_arithmetic_evolve_lora_Gemma2-9B.sh
+++ scripts/train_arithmetic_evolve_lora_Gemma2-9B.sh
@@ -5,7 +5,7 @@ CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0} python train_arithmetic.py \
   --adapter_type evolve_lora \
   --hf_fast_download --hf_preload --hf_prefer_safetensors "$@" \
   --model "google/gemma-2-9b" \
-  --evolve_rank_delay_ratio 0.3 \
+  --evolve_rank_delay_ratio 0.6 \
   --evolve_r_min 2 \
   --evolve_beta 0 \
   --evolve_alpha_max 0.001 \
