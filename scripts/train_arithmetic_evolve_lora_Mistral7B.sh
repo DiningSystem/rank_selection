6a242d8f4353a54be5e547622f0bb5ff58838167
DiningSystem <81498410+DiningSystem@users.noreply.github.com>
2026-07-21 15:38:35 -0500

    scripts_for_gemma2_8

--- scripts/train_arithmetic_evolve_lora_Mistral7B.sh
+++ scripts/train_arithmetic_evolve_lora_Mistral7B.sh
@@ -9,7 +9,7 @@ CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0} python train_arithmetic.py \
   --evolve_rank_delay_ratio 0.2 \
   --evolve_r_min 2 \
   --evolve_beta 0 \
-  --evolve_alpha_max 0.0005 \
+  --evolve_alpha_max 0.01 \
   --evolve_anneal_k 0.0 \
   --evolve_gate_floor 0.0 \
   --evolve_complexity_ema 0.9 \
