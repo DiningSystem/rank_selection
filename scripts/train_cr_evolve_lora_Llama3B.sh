b7489dadf9e0b5369c2eb063b8988699e34d57f9
DiningSystem <81498410+DiningSystem@users.noreply.github.com>
2026-08-19 15:48:50 +0700

    scripts_for_llama_3B_2

--- scripts/train_cr_evolve_lora_Llama3B.sh
+++ scripts/train_cr_evolve_lora_Llama3B.sh
@@ -12,8 +12,8 @@ CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0} python train_cr.py \
   --ortho_weight 1e-4 \
   --evolve_active_component_threshold 0.01 \
   --evolve_active_log_max_layers 6 \
-  --lora_r 22 \
-  --lora_alpha 44 \
+  --lora_r 21 \
+  --lora_alpha 42 \
   --lora_dropout 0.05 \
   --batch_size 6 \
   --grad_acc_steps 24 \
