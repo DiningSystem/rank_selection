8333706f08ecbc533b94ab60e8055f60db0a3ec5
DiningSystem <81498410+DiningSystem@users.noreply.github.com>
2026-09-09 13:07:21 +0700

    test_arithmetic_gemma

--- scripts/train_arithmetic.sh
+++ scripts/train_arithmetic.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=0 python train_arithmetic.py --lora_r=32 --lr=1e-3 --lora_alpha=32 --hf_fast_download --hf_preload --hf_prefer_safetensors
+CUDA_VISIBLE_DEVICES=0 python train_arithmetic.py --model="google/gemma-2-9b" --lora_r=32 --lr=1e-3 --lora_alpha=32 --hf_fast_download --hf_preload --hf_prefer_safetensors
 
