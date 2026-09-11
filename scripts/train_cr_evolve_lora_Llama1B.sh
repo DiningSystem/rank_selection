a842db67df456dc0c59607eab35980f757dd2db2
DiningSystem <81498410+DiningSystem@users.noreply.github.com>
2026-09-11 23:17:28 +0700

    test_cr_new_moe4

--- scripts/train_cr_evolve_lora_Llama1B.sh
+++ scripts/train_cr_evolve_lora_Llama1B.sh
@@ -5,7 +5,7 @@ CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0} python train_cr.py \
   --evolve_rank_delay_ratio 0 \
   --evolve_r_min 2 \
   --evolve_beta 0 \
-  --evolve_shared_rank 2 \
+  --evolve_shared_rank 3 \
   --evolve_alpha_max 1e-5 \
   --evolve_anneal_k 0 \
   --evolve_gate_floor 0 \
